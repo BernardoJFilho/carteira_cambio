be21213936132e1d491c20557caa41936350b084
BernardoJFilho <bernardojcm.filho@gmail.com>
2023-04-30 17:35:05 -0300

    termina questao6

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <header>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </header>
     );
   }
 }
